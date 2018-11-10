c5c8e710ff06f756f95000b316774eb8be2cf740
Ennio Gonzalez <mr.ennio@gmail.com>
2018-11-10 13:45:03 -0400

    modificacion url

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'enniogonzalez.github.io';
+$config['base_url'] = 'enniogonzalez.github.io/TallerUltimo';
 
 /*
 |--------------------------------------------------------------------------
